b0c88000af280082b60680ab015fe4e94f5bcb4c
lawvs <lawvs@users.noreply.github.com>
2018-09-05 16:42:07 +0800

    chore: polyfill

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,13 @@
 
 module.exports = {
   presets: [
-    [require('@babel/preset-env'), { modules: false }],
+    [
+      require('@babel/preset-env'),
+      {
+        modules: false,
+        useBuiltIns: 'usage', // import('core-js')
+      },
+    ],
     require('@babel/preset-react'),
   ],
   plugins: [require('@babel/plugin-proposal-class-properties')],
